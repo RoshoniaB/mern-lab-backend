bc36e58b08634cf1296dfb554e82632b137e86f5
roshoniab <roshoniabrooks@gmail.com>
2020-08-14 16:30:23 -0500

    added routes to songs.js

--- controllers/songs.js
+++ controllers/songs.js
@@ -1,7 +1,39 @@
-const express = require('express')
-const Song = require('../models/Song')
+const express = require('express');
+const Song = require('../models/Song');
+const router = express.Router();
 
+router.get('/', (req, res) => {
+	Song.find({}).then((song) => {
+		res.json(song);
+	});
+});
 
-const router = express.Router()
+router.post('/', (req, res) => {
+	let newSong = req.body;
+	Song.create(newSong).then(() => {
+		Song.find({}).then((songs) => {
+			res.json(songs);
+		});
+	});
+});
 
-module.exports = router
+router.put('/:id', (req, res) => {
+	let updateSong = req.body;
+	Song.findOneAndUpdate({ _id: req.params.id }, updateSong, {
+		new: true
+	}).then(() => {
+		Song.find({}).then((songs) => {
+			res.json(songs);
+		});
+	});
+});
+
+router.delete('/:id', (req, res) => {
+	Song.findOneAndDelete({ _id: req.params.id }).then(() => {
+		Song.find({}).then((songs) => {
+			res.json(songs);
+		});
+	});
+});
+
+module.exports = router;
